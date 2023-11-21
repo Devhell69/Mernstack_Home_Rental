b09351ca8404d4e17205245f1850a2d13cf261d5
Devhell69 <paelalit@gmail.com>
2023-11-21 23:03:49 +0700

    create sign in api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { signup } = require ('../controllers/auth.controller.js');
+const { signup, signin } = require ('../controllers/auth.controller.js');
 
 const router = express.Router();
 
 router.post("/signup", signup);
-// router.post("/signin", signin);
+router.post("/signin", signin);
 // router.post('/google', google);
 // router.get('/signout', signOut)
 
