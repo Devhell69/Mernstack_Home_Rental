668f1eae4155d256e13b0b1b2ab1a0f09c91dc99
Devhell69 <paelalit@gmail.com>
2023-11-23 02:26:33 +0700

    create user update api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -9,7 +9,34 @@ exports.test = (req, res) => {
   });
 };
 
-exports.updateUser = async (req, res, next) => {};
+exports.updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+  return next(errorHandler(401, 'You can only update your own account!'));
+try {
+  if (req.body.password) {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: req.body.avatar,
+      },
+    },
+    { new: true }
+  );
+
+  const { password, ...rest } = updatedUser._doc;
+
+  res.status(200).json(rest);
+} catch (error) {
+  next(error);
+}
+};
 
 exports.deleteUser = async (req, res, next) => {};
 
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,11 +6,12 @@ const {
   getUserListings,
   getUser,
 } = require("../controllers/user.controller.js");
+const { verifyToken } = require("../utils/verifyUser.js");
 
 const router = express.Router();
 
 router.get("/test", test);
-// router.post('/update/:id', verifyToken, updateUser)
+router.post("/update/:id", verifyToken, updateUser);
 // router.delete('/delete/:id', verifyToken, deleteUser)
 // router.get('/listings/:id', verifyToken, getUserListings)
 // router.get('/:id', verifyToken, getUser)
