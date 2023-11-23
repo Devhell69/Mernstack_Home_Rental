a2cc4bcdfa1241819098868dacffff72d8f22696
Devhell69 <paelalit@gmail.com>
2023-11-23 15:45:16 +0700

    add sign out functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { signup, signin, google } = require ('../controllers/auth.controller.js');
+const { google, signOut, signin, signup } = require ('../controllers/auth.controller.js');
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.post('/google', google);
-// router.get('/signout', signOut)
+router.get('/signout', signOut)
 
 module.exports = router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -112,6 +112,21 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch("/api/auth/signout");
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -185,10 +200,7 @@ export default function Profile() {
         >
           Delete account
         </span>
-        <span
-          //  onClick={handleSignOut}
-          className="text-red-700 cursor-pointer"
-        >
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
           Sign out
         </span>
       </div>
