87a157bad8048c14f39c62acc73fb643f641dfc5
Devhell69 <paelalit@gmail.com>
2023-11-23 20:07:04 +0700

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -50,6 +50,29 @@ exports.deleteUser = async (req, res, next) => {
   }
 };
 
-exports.getUserListings = async (req, res, next) => {};
+exports.getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
 
-exports.getUser = async (req, res, next) => {};
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) return next(errorHandler(404, "User not found!"));
+
+    const { password: pass, ...rest } = user._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
